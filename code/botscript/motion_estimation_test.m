6fa05fd91b2b9717fd1962da17dc279c08c6fabe
duhber <duhbera@gmail.com>
2015-02-10 12:02:53 +0530

    created the script for bot test using motion estimation

---
+++ code/botscript/motion_estimation_test.m
@@ -0,0 +1,31 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                               9 february 2015
+%
+%                     Motion-Estimation test on 3d-CAPTCHA
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%       
+%                               3D-CAPTCHA
+%   The Captcha consists of 2 frames taken from same 3D model but with
+%   different viewpoints. 
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+
+clc;
+close all;
+clear all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                              GLOBAL VARIABLES
+    numModel=1;
+    
+    modelDir='../frame/%d/frame_000%d.';
+    
+%     frame=sprintf(strcat(modelDir,'jpg'),model,0);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for model=1001:numModel+1000
+    
+    
+    
+end
