a619a524391463bcc24594759061e53c310ce525
duhber <duhbera@gmail.com>
2015-04-29 00:22:01 +0530

    add lines for selecting challenge point(randomly) amd cropping the patches around the challenge point

--- code/OpenGLobjloader/fast_script.sh
+++ code/OpenGLobjloader/fast_script.sh
@@ -2,7 +2,7 @@
 
 dir=$1
 stopPoint=$2
-modeldir="model"
+modeldir="testmodel"
 modeldir2="ocmodel"
 texdir="texture"
 texdir2="octexture"
@@ -55,6 +55,7 @@ for i in $(ls $modeldir|grep '\.obj$');do
 
         frame0="$dir2/frame_0000.p"
         keyimg="$dir2/frame_0000_keypts.jpg"
+        bgtex="$dir2/bgtexture.jpg"
 
         ./fast-Linux-x86_64  $img $keyimg
         ./fast-Linux-x86_64 -l $img $keypoint
@@ -71,6 +72,64 @@ for i in $(ls $modeldir|grep '\.obj$');do
 
 
         cat $keypoint >> $frame0
+        
+        RANGE=$numkeys
+
+        number=$RANDOM
+        let "number %= $RANGE"
+
+        #echo "$number"
+
+        line=$(sed -n "${number}p" "$keypoint")
+        
+        x=$(echo "$line"|awk 'BEGIN{FS=" "}{print $1}')
+        y=$(echo "$line"|awk 'BEGIN{FS=" "}{print $2}')
+        
+        #echo "$line"
+        #echo "$x"
+        #echo "$y"
+        
+        startx=$(($x-50))
+        starty=$(($y-50))
+
+        if [ $startx -lt 1 ]
+        then
+            startx=1
+        fi
+        
+        if [ $starty -lt 1 ]
+        then 
+            starty=1
+        fi 
+
+        ty=$(($starty+100))
+        tx=$(($startx+100))
+
+        if [ $ty -gt 399 ]
+        then
+            height=$((101-ty+399))
+        else
+            height=101
+        fi
+
+        if [ $tx -gt 599 ]
+        then 
+            width=$((101-tx+599))
+        else
+            width=101
+
+        fi
+
+        echo "$height"x
+
+        convert $img -crop "$height"x"$width"+"$startx"+"$starty" $bgtex
+        mogrify -format bmp $bgtex
+
+        if [ $? -ne 0 ]
+        then
+            continue
+        fi
+
 
 
         while true;do
